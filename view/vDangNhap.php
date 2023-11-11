0e57301766b25589c5a6cbaf5d062a87b3fad6b9
TuyenNeee <112460147+TranThiThanhTuyen02@users.noreply.github.com>
2023-11-11 15:23:19 +0700

    Add files via upload

--- view/vDangNhap.php
+++ view/vDangNhap.php
@@ -3,14 +3,106 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="shortcut icon" type="image/x-icon" href="./assets/img/logo.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/fontawesome.min.css"
-        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/dangnhap.css">
+    <link rel="stylesheet" href="../css/font-awesome.css">
     <title>Đăng Nhập</title>
+    <style>
+        * {
+          padding: 0;
+          margin: 0;
+          box-sizing: border-box;
+          font-family: Bookish;
+        }
+        body {
+        background-color: #dbf0ff;
+        }
+        #dangnhap {
+        background-color: #9dd1ff;
+        border-radius: 10px;
+        margin-top:50px;
+        }
+        #dangnhap h1 {
+        color: #03b8ff;
+        font-weight: bold;
+        margin: auto;
+        margin-bottom:50px;
+        }
+        .form {
+        width: 100%;
+        }
+        .form > div {
+        height: 4rem;
+        margin: 1.5rem 0;
+        }
+        .form__username,
+        .form__password {
+        display: flex;
+        width: 100%;
+        }
+        .form__icon {
+        font-size: 2rem;
+        background: #00415a;
+        width: 5rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        }
+        .form__icon i {
+        color: #ffffff;
+        }
+        .form__input {
+        width: 100%;
+        position: relative;
+        }
+        .form__input > input {
+        width: 100%;
+        height: 100%;
+        padding-left: 1rem;
+        font-size: 1.7rem;
+        color: rgb(0, 0, 0);
+        background-color: #ffffff;
+        transition: background-color 0.4s;
+        }
+        .form__input:hover > input {
+        background-color: #c6efff;
+        }
+
+        .form__submit button {
+        width: 100%;
+        height: 100%;
+        border: none;
+        border-radius: 0.5rem;
+        cursor: pointer;
+        outline: none;
+        transition: all 0.4s;
+        color: white;
+        background-color: #03b8ff;
+        
+        }
+        .form__submit button i {
+        color: #ffffff;
+        font-size: 3rem;
+        transition: all 0.4s;
+        }
+        .form__submit button:hover {
+        background-color: #51ceff;
+        transition: all 0.4s;
+        }
+        .form__submit button:hover i {
+        color: #ffffff;
+        transition: all 0.4s;
+        }
+        #togglePassword {
+        position: absolute;
+        font-size: 2.5rem;
+        color: #ffffff;
+        top: 50%;
+        right: 1rem;
+        transform: translate(0, -50%);
+        cursor: pointer;
+      }
+    </style>
 </head>
 <body>
     <section class="Form my-5 mx-5">
@@ -18,56 +110,50 @@
           <div class="row no-gutters"> 
 
             <div class="col-lg-5">
-              <img src="../assets/img/logo.png" class="img-fluid" alt="">
+              <img src="../img/logo.png" class="img-fluid" alt="">
               <h1 class="text-center">Phần mềm quản lý <br> tài chính cá nhân</h1>
             </div>
 
 
             <div class="col-lg-5 px-5 pt-5" id="dangnhap"> 
+              <h1  class="text-center">ĐĂNG NHẬP</h1>
 
-                <h1  class="text-center">ĐĂNG NHẬP</h1>
-
-                <form action="" class="form">
-
-                    <div class="form__username">
-                        <div class="form__username--icon form__icon">
-                            <i class="fa fa-user" aria-hidden="true"></i>
-                        </div>
-
+              <form action="" class="form">
+                <div class="form__username">
+                    <div class="form__username--icon form__icon">
+                      <i class="fa fa-user" aria-hidden="true"></i>
+                    </div>
                     <div class="form__username--input form__input">
-                        <input type="text" id="username" placeholder="Tên đăng nhập" onblur="checkUsername()">
-                        <span class="text-danger" id="err_user"></span>
+                        <input type="text" id="username" placeholder="Tên đăng nhập">
+                        <span></span>
                     </div>
+                </div>
 
-                        <p id="errorUsername"></p>
-                    <div class="form__password">
-
-                        <div class="form__password--icon form__icon">
-                            <i class="fa fa-lock" aria-hidden="true"></i>
-                        </div>
-
-                        <div class="form__password--input form__input">
-                            <input type="password" id="password" placeholder="Mật khẩu" onblur="checkPassword()">
-                            <span class="fa fa-eye" aria-hidden="true" id="togglePassword"></span>
-                            <span class="text-danger" id="err_password"></span>
-                        </div>
+                <div class="form__password">
+                    <div class="form__password--icon form__icon">
+                      <i class="fa fa-lock" aria-hidden="true"></i>
                     </div>
-                        <p id="errorPassword" class="text-center"></p>
-                        <span id="err" class="text-danger"></span>
-
-                    <div class="form__submit">
-                        <button type="button" class="font-weight-bold" onclick="login()">ĐĂNG NHẬP</button>
+                    <div class="form__password--input form__input">
+                        <input type="password" id="password" placeholder="Mật khẩu">
+                        <span class="fa fa-eye" aria-hidden="true" id="togglePassword"></span>
+                        <span></span>
                     </div>
-                </form>  
+                </div>
+
+                <div class="form__submit">
+                    <button type="button" class="font-weight-bold">ĐĂNG NHẬP
+                    </button>
+                </div>
+            </form>  
             <div class="text-center">
                 <a href="#">Quên mật khẩu</a>
-                <p class="sign-up">Chưa có tài khoản<a href="signup.html">&nbsp;Đăng ký tại đây</a></p>
+                <p class="dangky">Chưa có tài khoản<a href="danhgky.html">&nbsp;Đăng ký tại đây</a></p>
             </div>  
             </div>
           </div>
         </div>
     </section>
-    <!-- <script src="js/dangnhap.js"></script> -->
+    
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
             integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
         </script>
--- view/vTaiChinhHienTai.php
+++ view/vTaiChinhHienTai.php
@@ -1,61 +1,79 @@
-<!DOCTYPE html>
-<html lang="en">
+<style>
+    .main-title {
+        text-align: center;
+        font-weight: bold;
+        padding: 10px;
+        border-radius: 20px;
+    }
+    .main-title h4 {
+        border-radius: 30px;
+        width: 600px;
+    }
+    .sub-title {
+        text-align: center;
+        text-decoration: underline;
+        font-weight: bold;
+    }
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
-        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" href="../assets/css/taichinhhientai.css">
-    <title>Document</title>
-</head>
-<body>
-<div class="container">
+    .info-container {
+        background-color:#bfe1ff;
+        width: 500px;
+        margin: 0 auto;
+        margin-top: 20px;
+        margin-bottom: 30px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+        animation: slide-in 0.5s forwards;
+        position: relative;
+        border-radius: 0.5rem;
+    }
 
-        <?php require_once '../header.php'; ?>
+    .info-container h5 {
+        background-color: #ffffff;
+        border-radius: 30px;
+        width: 400px;
+        margin-top: 20px;
+    }
 
-<div class="body">
-    <section>
-        <div class="main-title d-flex justify-content-center mx-auto">
-            <h4 class="bg-primary p-2 text-white ">Tài Chính Hiện Tại</h4>
+    .info-item {
+        margin: 10px;
+        padding: 5px;
+        background-color: #ffffff;
+    }
+    .info-item p {
+        margin-left: 50px;
+    }
+
+    .more-link {
+        text-align: center;
+        margin-bottom:20px;
+    }
+
+</style>
+<section>
+    <div class="main-title d-flex justify-content-center mx-auto">
+        <h4 class="bg-primary p-2 text-white ">Tài Chính Hiện Tại</h4>
+    </div>
+
+    <div class="info-container">
+        <div class="sub-title d-flex justify-content-center mx-auto">
+            <h5 class="p-2">Tài Chính Hiện Tại: 5 000 000đ</h5>
         </div>
+        
+        <div class="info-box">
 
-
-        <div class="info-container">
-            <div class="sub-title d-flex justify-content-center mx-auto">
-                <h5 class="p-2">Tài Chính Hiện Tại: 5 000 000đ</h5>
+            <div class="info-item">
+                <p>Tiền mặt: 200 000đ</p>
             </div>
-            
-            <div class="info-box">
-
-                <div class="info-item">
-                    <p>Tiền mặt: 200 000đ</p>
-                </div>
-                <div class="info-item">
-                    <p>Thẻ tín dụng: 300 000đ</p>
-                </div>
-                <div class="info-item">
-                    <p>Tài khoản: 400 000đ</p>
-                </div>
-            
-                <div class="more-link">
-                    <a href="#">Xem thêm</a>
-                </div>
+            <div class="info-item">
+                <p>Thẻ tín dụng: 300 000đ</p>
             </div>
-
-            
-        </div>
+            <div class="info-item">
+                <p>Tài khoản: 400 000đ</p>
+            </div>
+        
+            <div class="more-link">
+                <a href="#">Xem thêm</a>
+            </div>
+        </div>  
     </div>
 </section>
-</div>
-        <?php require_once '../footer.php'; ?>
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
-            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-        </script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous">
-        </script>
-    </div>
-</body>
-
-</html>
