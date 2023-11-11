d3eb0f75d30105ac77927c4ef4085eb838652b17
MENNGUYEN0202 <112146123+MENNGUYEN0202@users.noreply.github.com>
2023-11-11 15:02:52 +0700

    Add files via upload

--- view/vCanhBao.php
+++ view/vCanhBao.php
@@ -1,82 +1,35 @@
 <?php
     require_once "controller/cCanhBao.php";
-    $c = new ControllerCanhBao();
-    $tbl = $c -> getAllCanhBao();
-    if($tbl){
-              if (mysql_num_rows($tbl)>0) {
-                $c = new ControllerCanhBao();
-                echo '<h4 class="ct-font text-center mt-5 mb-5 ">DANH SÁCH TÀI KHOẢN VƯỢT MỨC</h4>';
-                echo '<table class="table table-hover">';
-                echo '<thead>
-                        <tr>
-                          <th scope="col">Tên Tài Khoản</th>
-                          <th scope="col">Số Tiền</th>
-                          <th scope="col">Hạn Mức Chi Tiêu</th>
-                          <th scope="col">Số Tiền Vượt Quá Hạn Mức</th>
-                          <th scope="col">Hạng Mục Chi Nhiều Nhất</th>
-                        </tr>
-                      </thead>
-                      <tbody>';
-                  while ($row = mysql_fetch_assoc($tbl)) {
-                  echo '<tr>';
-                    echo '<th scope="row">'.$row['TenTK'].'</th>';
-                    echo '<td>'.$row['SoTienHM'].'</td>';
-                    echo '<td>'.$row['SoTien'].'</td>';
-                    echo '<td>'.$row['TongTienKhoanThu']-$row['SoTien'].'</td>';
-                    echo '<td>'.$row['HangMucN'].'</td>';
-                  echo '</tr>';
-                  }
-                echo '</tbody></table>';
-                }else{
-                    echo "0 Results";
-                }
-            }
-    else{
-      echo "ERROR";
-    }
 ?>
-<div>
-  <h4 class="ct-font text-center mt-5 mb-5 ">DANH SÁCH TÀI KHOẢN VƯỢT MỨC</h4>
-<table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">Tên Tài Khoản</th>
-      <th scope="col">Số Tiền</th>
-      <th scope="col">Hạn Mức Chi Tiêu</th>
-      <th scope="col">Số Tiền Vượt Quá Hạn Mức</th>
-      <th scope="col">Hạng Mục Chi Nhiều Nhất</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>4</td>
-      <td>@2</td>
-      <td>1</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>2</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>hihi</td>
-    </tr>
-    <tr>
-      <th scope="row">4</th>
-      <td>3</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>hihi</td>
-    </tr>
-    <tr>
-      <th scope="row">5</th>
-      <td>4</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>hihi</td>
-    </tr>
-    
-  </tbody>
-</table>
+<div class="container mt-3">
+  <h2>Tài Khoản Vượt Quá Chi Tiêu</h2>
+  <p></p>
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>STT</th>
+        <th>Tên Tài Khoản</th>
+        <th>Số tiền ban đầu(VND)</th>
+        <th>Số tiền hạn mức(VND)</th>
+        
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
 </div>
--- view/vDanhSachCacKhoanChiTieu.php
+++ view/vDanhSachCacKhoanChiTieu.php
@@ -1,106 +1,250 @@
+
+
 <body>
     <div class="container">
+       
         <div class="body">
-        <section>
-            <div class="row d-flex align-items-center justify-content-center">
-                <div class="col-md-8 d-flex align-items-center justify-content-center content mx-auto">
-                    <p class="bg-primary p-2">Danh Sách Các Khoản Chi Tiêu</p>
+            <section>
+                <div class="row d-flex align-items-center justify-content-center">
+                    <div class="col-md-8 d-flex align-items-center justify-content-center content mx-auto">
+                        <p class="bg-primary p-2">Danh Sách Các Khoản Chi Tiêu</p>
+                    </div>
+                </div>
+                <div class="body-kct">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">Hạng Mục</th>
+                                <th scope="col">Số Tiền </th>
+                                <th scope="col">Thời Gian</th>
+                                <th scope="col">Loại Tài Khoản </th>
+                                <th scope="col">Hoạt Động</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row">Ăn Uống</td>
+                                <td>4 500 000</td>
+                                <td>14/12/2023 - 13/04/2023</td>
+                                <td>Thẻ</td>
+                                <td>
+                                    <button type="button" data-toggle="modal" class="rs-btn"
+                                        data-target="#modal-sua">Sửa </button> /
+                                    <button type="button" data-toggle="modal" class="rs-btn"
+                                        data-target="#modal-xoa">xóa </button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td scope="row">Con Cái</td>
+                                <td>5 600 000</td>
+                                <td>14/12/2023 - 13/04/2023</td>
+                                <td>Thẻ</td>
+                                <td>
+                                    <button type="button" data-toggle="modal" class="rs-btn"
+                                        data-target="#modal-sua">Sửa </button> /
+                                    <button type="button" data-toggle="modal" class="rs-btn"
+                                        data-target="#modal-xoa">xóa </button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td scope="row">Sinh Hoạt</td>
+                                <td>2 400 000</td>
+                                <td>14/12/2023 - 13/04/2023</td>
+                                <td>Thẻ</td>
+                                <td>
+                                    <button type="button" data-toggle="modal" class="rs-btn"
+                                        data-target="#modal-sua">Sửa </button> /
+                                    <button type="button" data-toggle="modal" class="rs-btn"
+                                        data-target="#modal-xoa">xóa </button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td scope="row">Du Lịch</td>
+                                <td>3 400 000</td>
+                                <td>14/12/2023 - 13/04/2023</td>
+                                <td>Thẻ</td>
+                                <td>
+                                    <button type="button" data-toggle="modal" class="rs-btn"
+                                        data-target="#modal-sua">Sửa </button> /
+                                    <button type="button" data-toggle="modal" class="rs-btn"
+                                        data-target="#modal-xoa">xóa </button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td scope="row">Thuê Nhà</td>
+                                <td>45 005 000 000</td>
+                                <td>14/12/2023 - 13/04/2023</td>
+                                <td>Thẻ</td>
+                                <td>
+                                    <button type="button" data-toggle="modal" class="rs-btn"
+                                        data-target="#modal-sua">Sửa </button> /
+                                    <button type="button" data-toggle="modal" class="rs-btn"
+                                        data-target="#modal-xoa">xóa </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="row d-flex justify-content-end">
+                    <td>
+                        <button type="button" data-toggle="modal" class="rs-btn" data-target="#modal-them">Thêm
+                            Các Khoản Chi Tiêu</button>
+                    </td>
+                </div>
+            </section>
+
+            <!-- Modal Sua -->
+            <div class="modal fade" id="modal-sua" tabindex="-1" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Sửa Thông tin</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                        <form action="#">
+                            <div class="row form-group">
+                                <div class="col-4">
+                                    <label>Tên</label>
+                                </div>
+                                <div class="col-8">
+                                    <input type="text" class="form-control" id="txtMSSV" placeholder="">
+                                    <span id="errMSSV" class="text-danger font-italic">(*)</span>
+                                </div>
+                            </div>
+                        
+                            </div>
+                            <div class="row form-group">
+                                <div class="col-4">
+                                    <label for="">Số Tiền</label>
+                                </div>
+                                <div class="col-8">
+                                    <input type="text" name="" id="txtSDT" class="form-control" placeholder="">
+                                    <span id="errSDT" class="text-danger font-italic">(*)</span> 
+                                </div>
+                            </div>
+                            <div class="row form-group">
+                                <div class="col-4">
+                                    <label for="">Ngày Tháng Năm</label>
+                                </div>
+                                <div class="col-8">
+                                    <input type="date" name="" id="dateDK" class="form-control">
+                                    <span class="text-danger font-italic" id="errDateDK">(*)</span>
+                                </div>
+                            </div>
+                            <div class="row form-group">
+                                <div class="col-4">
+                                    <label for="">Hạng Mục</label>
+                                </div>
+                                <div class="col-8">
+                                    <select name="" id="listHoatDong" class="form-control">
+                                        <option value="" selected></option>
+                                        <option value="Thanh niên tình nguyện hè">Thanh niên tình nguyện hè</option>
+                                        <option value="Vệ sinh môi trường">Vệ sinh môi trường</option>
+                                        <option value="Tiếp sức mùa thi">Tiếp sức mùa thi</option>
+                                        <option value="Tình nguyện viên chống dịch">Tình nguyện viên chống dịch</option>
+                                    </select>
+                                </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Hủy</button>
+                            <button type="button" class="btn btn-primary">Lưu lại</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="container">
-            <table class="table table-bordered">
-  <thead>
-    <tr>
-      <th scope="col">Hạng Mục</th>
-      <th scope="col">Số Tiền </th>
-      <th scope="col">Thời Gian</th>
-      <th scope="col">Loại Tài Khoản </th>
-      <th scope="col">Hoạt Động</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td scope="row">Ăn Uống</td>
-      <td>4 500 000</td>
-      <td>14/12/2023 - 13/04/2023</td>
-      <td>Thẻ</td>
-      <td>
-        <button type="button" data-toggle="modal" class="rs-btn" data-target="#exampleModalCenter">Sửa </button> /
-        <button type="button" data-toggle="modal" class="rs-btn" data-target="#exampleModalCenter">xóa </button>
-        </td>
-    </tr>
-    <tr>
-      <td scope="row">Con Cái</td>
-      <td>5 600 000</td>
-      <td>14/12/2023 - 13/04/2023</td>
-      <td>Thẻ</td>
-      <td>
-        <button type="button" data-toggle="modal" class="rs-btn" data-target="#exampleModalCenter">Sửa </button> /
-        <button type="button" data-toggle="modal" class="rs-btn" data-target="#exampleModalCenter">xóa </button>
-        </td>
-    </tr>
-    <tr>
-      <td scope="row">Sinh Hoạt</td>
-      <td>2 400 000</td>
-      <td>14/12/2023 - 13/04/2023</td>
-      <td>Thẻ</td>
-      <td>
-        <button type="button" data-toggle="modal" class="rs-btn" data-target="#exampleModalCenter">Sửa </button> /
-        <button type="button" data-toggle="modal" class="rs-btn" data-target="#exampleModalCenter">xóa </button>
-        </td>
-    </tr>
-    <tr>
-      <td scope="row">Du Lịch</td>
-      <td>3 400 000</td>
-      <td>14/12/2023 - 13/04/2023</td>
-      <td>Thẻ</td>
-      <td>
-        <button type="button" data-toggle="modal" class="rs-btn" data-target="#exampleModalCenter">Sửa </button> /
-        <button type="button" data-toggle="modal" class="rs-btn" data-target="#exampleModalCenter">xóa </button>
-        </td>
-    </tr>
-    <tr>
-      <td scope="row">Thuê Nhà</td>
-      <td>45 005 000 000</td>
-      <td>14/12/2023 - 13/04/2023</td>
-      <td>Thẻ</td>
-      <td>
-        <button type="button" data-toggle="modal" class="rs-btn" data-target="#exampleModalCenter">Sửa </button> /
-        <button type="button" data-toggle="modal" class="rs-btn" data-target="#exampleModalCenter">xóa </button>
-        </td>
-    </tr>
-  </tbody>
-</table>
-            </div>
-        <div class="row d-flex justify-content-end">
-        <td>
-        <button type="button" data-toggle="modal" class="rs-btn" data-target="#exampleModalCenter">Thêm Các Khoản Chi Tiêu</button>
-        </td>
-        </div>
-        </section>
 
-        <!-- Modal Sua -->
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
+            <!-- Modal Xóa -->
+            <div class="modal fade" id="modal-xoa" tabindex="-1" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Thông Báo</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <p>Bạn có chắc muốn xóa khoản chi tiêu này không ?</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Hủy</button>
+                            <button type="button" class="btn btn-primary">Xóa</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="modal-body">
-                ...
+
+            <!-- Modal Thêm -->
+            <div class="modal fade" id="modal-them" tabindex="-1" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Tạo Khoản chi tiêu</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                        <form action="#">
+                            <div class="row form-group">
+                                <div class="col-4">
+                                    <label>Tên</label>
+                                </div>
+                                <div class="col-8">
+                                    <input type="text" class="form-control" id="txtMSSV" placeholder="">
+                                    <span id="errMSSV" class="text-danger font-italic">(*)</span>
+                                </div>
+                            </div>
+                        
+                            </div>
+                            <div class="row form-group">
+                                <div class="col-4">
+                                    <label for="">Số Tiền</label>
+                                </div>
+                                <div class="col-8">
+                                    <input type="text" name="" id="txtSDT" class="form-control" placeholder="">
+                                    <span id="errSDT" class="text-danger font-italic">(*)</span> 
+                                </div>
+                            </div>
+                            <div class="row form-group">
+                                <div class="col-4">
+                                    <label for="">Ngày Tháng Năm</label>
+                                </div>
+                                <div class="col-8">
+                                    <input type="date" name="" id="dateDK" class="form-control">
+                                    <span class="text-danger font-italic" id="errDateDK">(*)</span>
+                                </div>
+                            </div>
+                            <div class="row form-group">
+                                <div class="col-4">
+                                    <label for="">Hạng Mục</label>
+                                </div>
+                                <div class="col-8">
+                                    <select name="" id="listHoatDong" class="form-control">
+                                        <option value="" selected></option>
+                                        <option value="Thanh niên tình nguyện hè">Thanh niên tình nguyện hè</option>
+                                        <option value="Vệ sinh môi trường">Vệ sinh môi trường</option>
+                                        <option value="Tiếp sức mùa thi">Tiếp sức mùa thi</option>
+                                        <option value="Tình nguyện viên chống dịch">Tình nguyện viên chống dịch</option>
+                                    </select>
+                                </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Hủy</button>
+                            <button type="button" class="btn btn-primary">Lưu</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-            </div>
-        </div>
-        </div>
 
         </div>
+       
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
             integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
         </script>
@@ -109,3 +253,5 @@
         </script>
     </div>
 </body>
+
+</html>
