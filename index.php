2afd423e71a1816089133a785fd9d71ebe4ae32a
ducthinh1102 <ducthinh140202@gmail.com>
2023-11-11 16:40:55 +0700

    aaaa

--- index.php
+++ index.php
@@ -21,38 +21,37 @@
         case 'taikhoan':{
             require_once("view/vQLTaiKhoan.php");
             break;}
-        
         case 'home':{
             require_once("view/vHome.php");
             break;}
         default: {
             require_once("index.php");}
     }
-    include_once("View/vhangmucvahanmuc.php");
+    include_once("view/vhangmucvahanmuc.php");
             $p = new viewpro();
             //Hạng mục
             if(isset($_REQUEST['hangmuc'])){
                 echo "<button style='float: right;'><a href='index.php?addProd'  style='text-decoration:none; color:black;'>Thêm sản phẩm</a></button>"."<br>";
                 $p->viewadpro();
             }elseif(isset($_REQUEST['addProd'])){
-                include_once("View/vaddhangmuc.php");
+                include_once("view/vaddhangmuc.php");
             }elseif(isset($_REQUEST['Delpro'])){
-				include_once("View/vdelhangmuc.php");}
+				include_once("view/vdelhangmuc.php");}
             elseif(isset($_REQUEST['editpro'])){
-                include_once("View/vehangmuc.php");} 
+                include_once("view/vehangmuc.php");} 
  //Hạn mức
              if(isset($_REQUEST['hanmuc'])){
                     echo "<button style='float: right;'><a href='index.php?addProd1'  style='text-decoration:none; color:black'>Thêm hạn mức</a></button>"."<br>";
                     $p->viewadpro();
                 }elseif(isset($_REQUEST['addProd1'])){
-                    include_once("View/vhanmuc.php");
+                    include_once("view/vhanmuc.php");
                 }elseif(isset($_REQUEST['Delhm'])){
-                    include_once("View/vdelhanmuc.php");}
+                    include_once("view/vdelhanmuc.php");}
                 elseif(isset($_REQUEST['edithm'])){
-                    include_once("View/vehanmuc.php");}
+                    include_once("view/vehanmuc.php");}
    if(isset($_REQUEST['vtinhhinh'])){
-                include_once("View/vtinhhinh.php");
+                include_once("view/vtinhhinh.php");
                     }elseif(isset($_REQUEST['xem'])){
-                    include_once("View/vtinhhinh.php");}
+                    include_once("view/vtinhhinh.php");}
     require_once 'layout/footer.php';
 ?>
